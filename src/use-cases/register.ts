bda9aa98ffe59fdaa6f95ee28fbf9171a7bee215
Douglas Oliveira <douglaspo_97@outlook.com>
2023-11-04 09:26:36 -0300

    feat: add global error handler

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,4 @@
 import { UsersRepository } from '@/repositories/users-repository'
-import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUseCaseRequest {
@@ -12,7 +11,7 @@ export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
-    const password_hash = await hash(password, 6)
+    const password_hash = await hasth(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
