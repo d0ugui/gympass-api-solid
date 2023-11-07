d94f0263d194275732a0ee21f608e408b351ddc2
Douglas Oliveira <douglaspo_97@outlook.com>
2023-11-07 18:26:07 -0300

    feat: add create-gym use-case

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create a gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'JavaScript Gym',
+      description: null,
+      phone: null,
+      latitude: -23.5535016,
+      longitude: -46.6222736,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
---
+++ src/use-cases/create-gym.ts
@@ -0,0 +1,36 @@
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { Gym } from '@prisma/client'
+
+interface CreateGymRequest {
+  title: string
+  description: string | null
+  phone: string | null
+  latitude: number
+  longitude: number
+}
+
+interface CreateGymResponse {
+  gym: Gym
+}
+
+export class CreateGymUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  }: CreateGymRequest): Promise<CreateGymResponse> {
+    const gym = await this.gymsRepository.create({
+      title,
+      description,
+      phone,
+      latitude,
+      longitude,
+    })
+
+    return { gym }
+  }
+}
