73bf711cf00ec8677747ffcb9eb3733310d606f4
Douglas Oliveira <douglaspo_97@outlook.com>
2023-11-08 11:19:41 -0300

    feat: create fetch-nearby-gyms use-case

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ GymPass style app.
 - [x] Deve ser possível obter o perfil de um usuário logado;
 - [x] Deve ser possível obter o número de check-ins realizados pelo usuário logado;
 - [x] Deve ser possível o usuário obter seu histórico de check-ins;
-- [ ] Deve ser possível o usuário buscar academias pŕoximas;
-- [ ] Deve ser possível o usuário buscar academias pelo nome;
+- [ ] Deve ser possível o usuário buscar academias pŕoximas até 10km;
+- [x] Deve ser possível o usuário buscar academias pelo nome;
 - [x] Deve ser possível o usuário realizar check-in em uma academia;
 - [ ] Deve ser possível validar o check-in de um usuário;
 - [x] Deve ser possível cadastrar uma academia;
---
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: FetchNearbyGymsUseCase
+
+describe('Fetch Nearby Gyms Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new FetchNearbyGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to fetch nearby gyms', async () => {
+    await gymsRepository.create({
+      title: 'Near Gym',
+      description: null,
+      phone: null,
+      latitude: -23.5535016,
+      longitude: -46.6222736,
+    })
+
+    await gymsRepository.create({
+      title: 'Far Gym',
+      description: null,
+      phone: null,
+      latitude: -23.5047673,
+      longitude: -46.4460398,
+    })
+
+    const { gyms } = await sut.execute({
+      userLatitude: -23.5535016,
+      userLongitude: -46.6222736,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })])
+  })
+})
